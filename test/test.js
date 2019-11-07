4a2cd1421ca77fedcc8076d09ad17d6c12ba4994
lilindog <lilin@lilin.site>
2019-11-07 23:35:12 +0800

    优化字符树形生成符号

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@
 const sdtree = require("../index");
 const fs = require("fs");
 
-let path = "../react-test", options = {comment: "", exclude: []};
+let path = "../../sdtree", options = {comment: "", exclude: []};
 
 fs.writeFileSync("test.text", sdtree.getTreeStr(path, options));
 fs.writeFileSync("test.json", JSON.stringify(sdtree.getTreeJson(path, options), null, 4));
