e28de60b8285d28c433d21a0e455c2be4c55b795
lilindog <lilin@lilin.site>
2020-01-19 10:18:49 +0800

    增加str输出的powered by

--- test/test.js
+++ test/test.js
@@ -5,7 +5,9 @@
 const sdtree = require("../index");
 const fs = require("fs");
 
-let path = "../../sdtree", options = {comment: "", exclude: [".git", "node_modules"]};
+const
+path = "../../sdtree", 
+options = {comment: "", exclude: [".git", "node_modules"]};
 
 fs.writeFileSync("test.text", sdtree.getTreeStr(path, options));
 fs.writeFileSync("test.json", JSON.stringify(sdtree.getTreeJson(path, options), null, 4));
