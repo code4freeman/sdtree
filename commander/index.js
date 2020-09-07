645ac47882c427157013f8101598664d015c335a
lilindog <lilin@lilin.site>
2020-09-07 13:51:30 +0800

    增加default帮助信息打印选项

--- commander/index.js
+++ commander/index.js
@@ -1,13 +1,27 @@
 #! /usr/bin/env node
 
-const  { cmd } = require("cmdhelper");
+const { cmd } = require("cmdhelper");
+const { version } = require("../package.json");
+
+const 
+versionDescript = "[--v, version] `显示当前版本`",
+helpDescript    = "[--h, help]    `显示帮助信息`";
 
 [
     require("../modules/default")
 ].forEach(module => {
     if (!module.name) {
-        //。。。这里处理default --v --h 等命令
-        cmd(module.descripts, module.action);
+        cmd([versionDescript, helpDescript, ...module.descripts], (p) => {
+            if (p.version) {
+                console.log(`\n    当前版本：${version}\n`);
+            }
+            else if (p.help) {
+                console.log("\n    " + module.descripts.join("\n    ") + "\n");
+            }
+            else {
+                module.action(p);
+            }
+        });
     }
     else {
         cmd(module.name, module.descripts, module.action);
